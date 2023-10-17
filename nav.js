a91e69ff689cfcdcb4cabe495ea178924a721244
santosgonz <130101203+santosgonz@users.noreply.github.com>
2023-10-17 14:46:08 -0700

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,47 @@
+"use strict";
+
+/******************************************************************************
+ * Handling navbar clicks and updating navbar
+ */
+
+/** Show main list of all stories when click site name */
+
+function navAllStories(evt) {
+  console.debug("navAllStories", evt);
+  hidePageComponents();
+  putStoriesOnPage();
+}
+
+$body.on("click", "#nav-all", navAllStories);
+
+/** Show login/signup on click on "login" */
+
+function navLoginClick(evt) {
+  console.debug("navLoginClick", evt);
+  hidePageComponents();
+  $loginForm.show();
+  $signupForm.show();
+}
+
+$navLogin.on("click", navLoginClick);
+
+/** When a user first logins in, update the navbar to reflect that. */
+
+function updateNavOnLogin() {
+  console.debug("updateNavOnLogin");
+  $(".main-nav-links").show();
+  $navLogin.hide();
+  $navLogOut.show();
+  $navUserProfile.text(`${currentUser.username}`).show();
+}
+
+function updateSubmit(evt){
+  // alert("CLICK")
+  console.debug("updateSubmit", evt);
+  hidePageComponents();
+  //put all stories on?
+  $allStoriesList.show();
+  $submitForm.show();
+}
+//why is form not working???1111111
+$navSubmit.on("click", updateSubmit);
---
+++ stories.js
@@ -0,0 +1,73 @@
+"use strict";
+
+// This is the global list of the stories, an instance of StoryList
+let storyList;
+
+/** Get and show stories when site first loads. */
+
+async function getAndShowStoriesOnStart() {
+  storyList = await StoryList.getStories();
+  $storiesLoadingMsg.remove();
+
+  putStoriesOnPage();
+}
+
+/**
+ * A render method to render HTML for an individual Story instance
+ * - story: an instance of Story
+ *
+ * Returns the markup for the story.
+ */
+
+function generateStoryMarkup(story) {
+  // console.debug("generateStoryMarkup", story);
+
+  const hostName = story.getHostName();
+  return $(`
+      <li id="${story.storyId}">
+        <a href="${story.url}" target="a_blank" class="story-link">
+          ${story.title}
+        </a>
+        <small class="story-hostname">(${hostName})</small>
+        <small class="story-author">by ${story.author}</small>
+        <small class="story-user">posted by ${story.username}</small>
+      </li>
+    `);
+}
+
+/** Gets list of stories from server, generates their HTML, and puts on page. */
+
+function putStoriesOnPage() {
+  console.debug("putStoriesOnPage");
+
+  $allStoriesList.empty();
+
+  // loop through all of our stories and generate HTML for them
+  for (let story of storyList.stories) {
+    const $story = generateStoryMarkup(story);
+    $allStoriesList.append($story);
+  }
+
+  $allStoriesList.show();
+}
+
+async function submitNewStory(evt) {
+  console.debug("submitNewStory");
+  evt.preventDefault();
+
+  //Get value from form and combine them
+  const title = $("#create-title").val();
+  const url = $("#create-url").val();
+  const author = $("#create-author").val();
+  const username = currentUser.username
+  const storyData = {title, url, author, username };
+
+  const story = await storyList.addStory(currentUser, storyData);
+
+  const $story = generateStoryMarkup(story);
+  //how to make it at top?
+  $allStoriesList.prepend($story);
+
+}
+
+$submitForm.on("submit", submitNewStory);
